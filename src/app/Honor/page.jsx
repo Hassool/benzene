9e57ccf99aa6af61587f911c874b05fa5e9aef72
Hassool <mekkyot@gmail.com>
2025-10-20 17:59:54 +0100

    add honor

---
+++ src/app/Honor/page.jsx
@@ -0,0 +1,11 @@
+// Honor/page.jsx
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+export default function page() {
+  const router = useRouter();
+  useEffect(() => {
+    router.replace('/'); // redirect to home
+  }, []);
+  return null;
+}
